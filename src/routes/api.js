f7af801ffc239a3dc13abb7291ae6eca9796f563
Rajesh-107 <applerb326@gmail.com>
2022-10-19 13:12:52 +0600

    task controller and api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 //taskmanager1234
 const express = require("express");
 const UsersController = require("../controller/UsersController")
+const TasksController = require("../controller/TasksController")
 const AuthVerifyMiddleware = require("../middleware/AuthVerifyMiddleware")
 
 const router = express.Router();
@@ -10,5 +11,13 @@ router.post("/registartion", UsersController.registartion)
 router.post("/UserLogin", UsersController.UserLogin)
 
 router.post("/profileUpdate",AuthVerifyMiddleware, UsersController.profileUpdate)
+
+// about task 
+router.post("/createTask",AuthVerifyMiddleware, TasksController.createTask)
+router.post("/deleteTask",AuthVerifyMiddleware, TasksController.deleteTask)
+router.get("/updateTaskStatus/:id/:status",AuthVerifyMiddleware, TasksController.updateTaskStatus)
+router.get("/listTaskByStatus/:status",AuthVerifyMiddleware, TasksController.listTaskByStatus)
+
+
 
 module.exports = router;
