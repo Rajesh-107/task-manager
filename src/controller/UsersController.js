8398be103111fb462a989bc1fb6ce96e5f9dde7d
Rajesh-107 <applerb326@gmail.com>
2022-10-23 19:37:57 +0600

    status 200

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -5,7 +5,7 @@ exports.registartion=(req, res) => {
     let reqBody = req.body;
     UsersModel.create(reqBody, (err, data) => {
         if(err){
-            res.status(400).json({status:"fail", data:err})
+            res.status(200).json({status:"fail", data:err})
         }
         else{
             res.status(200).json({status:"success", data:data})
